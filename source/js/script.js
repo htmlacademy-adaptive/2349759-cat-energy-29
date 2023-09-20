a9357af8f9aa55e5257090cb04cbe75d10acf4fe
Shopsha <anast.shopsha@icloud.com>
2023-09-20 19:34:53 +0300

    Внесла правки

--- source/js/script.js
+++ source/js/script.js
@@ -4,7 +4,7 @@ let navToggle = document.querySelector('.main-header__toggle');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navList.classList.contains('main-nav--close-state')) {
+  if (navMain.classList.contains('main-nav--close-state')) {
     navMain.classList.remove('main-nav--close-state');
     navMain.classList.add('main-nav--open-state');
     navToggle.classList.add('main-header__toggle--open-state');
